b1993d42d9bdb164b1f33164a520736ac7d04afa
Abraham Vazquez <mabraham.vazquez@gmail.com>
2016-05-04 03:23:08 -0500

    Compilada Exitosa, se agrega la función Pulso que contiene la comparación de los valores QS

---
+++ README.txt
@@ -0,0 +1,2 @@
+
+Version 2.0.2 del Dispositivo, el sensor de pulsos funciona, sin embargo es considerable que se acondicione ya sea con el velcro para el dedo o con el sujetador para el lóbulo de la oreja.
--- VestibleTT2.0.2.ino
+++ VestibleTT2.0.2.ino
@@ -1,18 +1,4 @@
 
-/*  Pulse Sensor Amped 1.4    by Joel Murphy and Yury Gitman   http://www.pulsesensor.com
-
-----------------------  Notes ----------------------  ---------------------- 
-This code:
-1) Blinks an LED to User's Live Heartbeat   PIN 13
-2) Fades an LED to User's Live HeartBeat
-3) Determines BPM
-4) Prints All of the Above to Serial
-
-Read Me:
-https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino/blob/master/README .md   
- ----------------------       ----------------------  ----------------------
-*/
-
 //  Variables
 int pulsePin = A0;                 // Pulse Sensor purple wire connected to analog pin 0
 int blinkPin = 13;                // pin to blink led at each beat
@@ -46,8 +32,24 @@ void loop()
 {
   
     serialOutput() ;       
-    
-  if (QS == true){     // A Heartbeat Was Found
+
+    Pulso();
+                            //  take a break
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void ledFadeToBeat()
+{
+    fadeRate -= 15;                         //  set LED fade value
+    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
+    analogWrite(fadePin,fadeRate);          //  fade LED
+  }
+  
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+void Pulso()
+{
+    if (QS == true)
+    {     // A Heartbeat Was Found
                        // BPM and IBI have been Determined
                        // Quantified Self "QS" true when arduino finds a heartbeat
         fadeRate = 255;         // Makes the LED Fade Effect Happen
@@ -57,19 +59,8 @@ void loop()
   }
      
   ledFadeToBeat();                      // Makes the LED Fade Effect Happen 
-  delay(20);                             //  take a break
+  delay(20); 
 }
-
-
-
-
-
-void ledFadeToBeat(){
-    fadeRate -= 15;                         //  set LED fade value
-    fadeRate = constrain(fadeRate,0,255);   //  keep LED fade value from going into negative numbers!
-    analogWrite(fadePin,fadeRate);          //  fade LED
-  }
-
 
 
 
