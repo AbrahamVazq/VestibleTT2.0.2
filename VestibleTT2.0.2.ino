1f4ff7470f97d8138ec473840e42dd4c453953d9
Abraham Vazquez <mabraham.vazquez@gmail.com>
2016-05-04 12:50:30 -0500

    Se agrega las funciones temperatura, e interrumpe, código compilado

--- VestibleTT2.0.2.ino
+++ VestibleTT2.0.2.ino
@@ -1,13 +1,24 @@
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <SPI.h>
+#include <SdFat.h>
+#include <Time.h>
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*Creamos objetos de la clase SdFat */
+SdFat sd;
+SdFile archivo;
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 //  Variables
-int pulsePin = A0;                 // Pulse Sensor purple wire connected to analog pin 0
+int pulsePin = A0;                // Pulse Sensor purple wire connected to analog pin 0
+int tempPin = A2;                 // Definimos la entrada anlogica en pin A0 
 int blinkPin = 13;                // pin to blink led at each beat
 int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
 int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
-
+float tempC;                      // variable para el dato del Sensor
+const int chipSelect = 4;         // Pin para el ChipSelect
+const int botonPin = 8;           // Boton de Pausa para separar datos
+int botonEstado = 0;              //Este es el estado inicial del boton de pausa
+int turno;
+                
 // Volatile Variables, used in the interrupt service routine!
 volatile int BPM;                   // int that holds raw Analog in 0. updated every 2mS
 volatile int Signal;                // holds the incoming raw data
@@ -37,8 +48,16 @@ void setup(){
 void loop()
 {
   
-    serialOutput() ;       
-    
+    serialOutput() ;   
+    delay(200);
+    switch(turno)
+    {
+      case 1:
+            temperatura();
+      case 2:
+            pulso();
+    }
+    delay(20);
   
 }
 
@@ -67,5 +86,69 @@ void pulso()
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void temperatura()
+{
+   /*Creamos una objeto t de la clase tiempo */
+    time_t t = now();
+  /* Leemos el valor desde el sensor */
+    tempC = analogRead(tempPin);
+
+  /* Convierte el valor a temperatura*/
+    tempC = (5.0 * tempC * 100.0)/1024.0;
+  /* Datos que se mostraran en consola serial*/
+    Serial.print(tempC);
+    Serial.print(" Grados Celsius a las ");
+    Serial.print(day(t));
+    Serial.print(+ "-");
+    Serial.print(month(t));
+    Serial.print(+ "-");
+    Serial.print(year(t));
+    Serial.print(+ " ");
+    Serial.print(hour(t));
+    Serial.print(+ ":");
+    Serial.print(minute(t));
+    Serial.print(+ ":");
+    Serial.print(second(t));
+    Serial.print("\n");
+    digitalWrite(3, HIGH);
+    
+   /* Re-abrimos el archivo para lectura */
+  if (!archivo.open("Temperatura.txt", O_RDWR | O_CREAT | O_AT_END)) 
+  {
+    sd.errorHalt("opening test.txt for write failed");
+  }
+  
+  /*Si el archivo se abrio correctamente, escribimos en el */
+  Serial.print("La lectura sensa: ");
+  archivo.print("\"");
+  archivo.print(hour(t));
+  archivo.print(+ ":");
+  archivo.print(minute(t));
+  archivo.print(+ ":");
+  archivo.print(second(t));
+  archivo.print("\"");
+  archivo.print(+ " ");
+  archivo.print(tempC);
+  archivo.println(+ ",");
+
+  archivo.close();
+   delay(1000);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void interrumpe()
+{
+    digitalWrite(7, HIGH);
+    delay(500);
+    digitalWrite(7,LOW);
+    Serial.println(+ "]");
+    Serial.println(+ "[");
+    archivo.println(+ "]");
+    archivo.println(+ "[");
+    delay(5000);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
